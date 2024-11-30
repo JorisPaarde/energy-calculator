2b242a459e86af213d30f305e12e637cb439b872
JorisPaarde <jorispaarde@gmail.com>
2024-11-30 23:50:43 +0100

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/energy-calculator/',
   plugins: [],
   server: {
     host: '0.0.0.0',
-    hmr: true, // Change this line to false disable auto-refreshing.
+    hmr: true,
   }
 })
